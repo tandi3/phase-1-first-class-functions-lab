9f2551d04b85cf4c804471f68ee47bcbf15d65a3
tandie oyucho <ysoyucho@gmail.com>
2025-03-20 10:23:07 +0300

    added changes

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const drivers =['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function returnFirstTwoDrivers(drivers){
+    return drivers.slice(0,2);
+}
+ function returnLastTwoDrivers(drivers) {
+     return drivers.slice(-2);
+ }
+    const selectingDrivers = [returnFirstTwoDrivers,        
+    returnLastTwoDrivers];
+
+    function createFareMultiplier(mulitiplier){
+    return function(fare){
+        return fare * mulitiplier;
+    }
+}
+function fareDoubler(fare){
+    return fare * 2;
+}
+function fareTripler(fare){
+    return fare * 3;
+}
+function selectDifferentDrivers(drivers, selectDifferentDrivers){
+    return selectDifferentDrivers(drivers);
+}
